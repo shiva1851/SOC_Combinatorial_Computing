2e4b71ac477836af48eeff1653e529ea7106525e
SUSMIT NEOGI <120775551+shiva1851@users.noreply.github.com>
2023-06-24 21:29:02 +0530

    2_player_game.cpp

--- Week-2/2_player_game.cpp
+++ Week-2/2_player_game.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-
+// we try to traverse through all possible moves of player 1 at any grid and then assess possible moves that player 2 can make according to those of player 1.
+// among them, we try to choose the one which has maximum weight.
+// thus we keep on adding the max possible weights for player 2.
+// after the last move, if the weights of player 2 is greater than that of player 1 in all the cases, then we say that player 2 has winning strategy.
+// negation: if atleast one case exists where weights of player 1 is greater than palyer 2, then because player 1 makes first move, player 1 has winning strategy.
 int solve(int n, int m, vector<vector<int>>& w) {
   vector<vector<int>> T(n, vector<int>(m + 1));
   for (int i = 0; i < n; i++) {
@@ -27,6 +30,7 @@ return result;
 int main() {
   int n, m;
   cin >> n >> m;
+// store weights in the vector
   vector<vector<int>> w(n * (n - 1) / 2);
   for (int i = 0; i < int(w.size()); i++) {
   w[i].resize(m + 1);
